dd3643e6a5577d95aef7fc2686c68d193e517a59
Student-Ruqayya <ruqayya.hasnee@gmail.com>
2025-07-20 12:55:41 +0500

    api integration

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Weather.css";
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.webp";
@@ -8,8 +8,51 @@ import drizzle_icon from "../assets/drizzle.png";
 import rain_icon from "../assets/rain.png";
 import humidity_icon from "../assets/humidity.png";
 import snow_icon from "../assets/snow.png";
+import { act } from "react";
 
 function Weather() {
+  const [weatherData, setWeatherData] = useState(false);
+  const allIcons = {
+    "01d": clear_icon,
+    "01n": clear_icon,
+    "02d": cloud_icon,
+    "02n": cloud_icon,
+    "03d": cloud_icon,
+    "03n": cloud_icon,
+    "04d": drizzle_icon,
+    "04n": drizzle_icon,
+    "09d": rain_icon,
+    "09n": rain_icon,
+    "10d": rain_icon,
+    "10n": rain_icon,
+    "13d": snow_icon,
+    "13n": snow_icon,
+  };
+
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  useEffect(() => {
+    search("Delhi");
+  }, []);
   return (
     <div className="weather">
       <div className="search-bar">
@@ -18,21 +61,21 @@ function Weather() {
       </div>
 
       <img src={clear_icon} alt="" className="weather-icon" />
-      <p className="temperature">25°C</p>
-      <p className="location">City Name</p>
+      <p className="temperature">{weatherData.temperature}°C</p>
+      <p className="location">{weatherData.location}</p>
       <div>
         <div className="weather-data">
           <div className="col">
             <img src={humidity_icon} alt="" />
             <div>
-              <p>67 %</p>
+              <p>{weatherData.humidity} %</p>
               <span>Humidity</span>
             </div>
           </div>
           <div className="col">
             <img src={wind_icon} alt="" />
             <div>
-              <p>15 Km/h</p>
+              <p>{weatherData.windSpeed} Km/h</p>
               <span>Wind Speed</span>
             </div>
           </div>
